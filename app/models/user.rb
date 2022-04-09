494f1920a29377f2b8e62b861295e27a97e8ef8a
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-04-09 09:11:29 +0000

    [Add]いいね機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, lenght: {maximum: 50 }
+  validates :introduction, length: { maximum: 50 }
 
 
 
